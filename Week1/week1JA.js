4c3be6e7e8c5b0e14a42889efa50d882e473e88a
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-05-27 23:20:45 +0530

    Add files via upload

---
+++ Week1/week1JA.js
@@ -0,0 +1,100 @@
+
+
+// console.log(typeof null);
+
+// let a; // undefined
+// console.log(a + 1);
+
+// console.log(typeof NaN);
+
+
+// //NaN is "Not A Number" => a special numeric value represting an invalid mathematical operation.
+
+// console.log(typeof []); // array
+// console.log(typeof {}); // object
+
+// //From JS perspective array is also considered as a type of object
+
+// [1,2,3,4]
+
+// console.log(Array.isArray([1,2,3,4])) // syntax to check if the assigned value to a variable is array or not
+
+
+console.log(a); // ✅ undefined (hoisted + initialized)
+var a = 10
+console.log(a); // 10
+
+
+
+//c#, java 
+// console.log(x) // O/P => Error 
+// int x
+
+
+//javascript  => when it was a scripting language
+// console.log(x) // O/P => Here the x value is pointing towards a Undefined
+// var x
+
+//javascript 
+// console.log(x) // O/P =>  Error // Here the x value is pointing towards a Temporal Dead Zone
+// let x
+
+//javascript 
+// console.log(x) // O/P =>  Error // Here the x value is pointing towards a Temporal Dead Zone
+// const x
+
+
+//Sayhello() // named function => hoisting
+
+//function expression => Hoisting takes place bit returns error
+// x()
+// let x = function () {
+//     console.log("Welcome")
+// }
+
+//const declaration using object
+
+// const x =""
+// const y =798
+
+
+// const user = {
+//     name: "John",
+//     mob: 89789789
+// };
+
+// user.name = "David";
+// console.log(user.name);
+
+
+// const user = {
+//     name: "John",
+//     mob: 89789789
+// };
+
+// const user = {
+//     name: "David",
+//     mob: 89789789
+// };
+
+
+// user.name = "David";
+// console.log(user.name);
+
+
+// console.log(1===true) // true 
+
+// while (paginationbutton.isEnabled) {  //
+//     console.log("While loop");
+//    // pagination action
+    
+// }
+
+
+console.log(1 + "1");  // Here "+" is a concatenation operator
+
+console.log("5" - 2); // 3 
+
+console.log(true + true); // 2
+
+console.log([] == false); // true
